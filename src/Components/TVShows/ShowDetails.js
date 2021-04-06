e84f5951bb4888a757167e1b9634ac001271d549
diganta15 <ddas461598@gmail.com>
2021-04-06 21:21:20 +0530

    Added details page for TV shows

---
+++ src/Components/TVShows/ShowDetails.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Container from "./Container";
+
+class ShowDetails extends Component {
+  state = {
+    data: {
+      actorList: [{}],
+    },
+  };
+  componentDidMount() {
+    const id = this.props.match.params.id;
+
+    axios
+      .get(`https://imdb-api.com/en/API/Title/k_83ul3o8k/${id}`)
+      .then((res) => this.setState({ data: res.data }));
+  }
+
+  render() {
+    const {
+      fullTitle,
+      year,
+      releaseDate,
+      runtimeStr,
+      plot,
+      image,
+      stars,
+      actorList,
+      tagline,
+      imDbRating,
+    } = this.state.data;
+    return (
+      <div>
+        <Container
+          fullTitle={fullTitle}
+          year={year}
+          releaseDate={releaseDate}
+          runtimeStr={runtimeStr}
+          plot={plot}
+          image={image}
+          stars={stars}
+          actorList={actorList}
+          tagline={tagline}
+          imDbRating={imDbRating}
+        />
+      </div>
+    );
+  }
+}
+
+export default ShowDetails;
